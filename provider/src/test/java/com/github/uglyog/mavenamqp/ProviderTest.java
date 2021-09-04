d855cf44d506417a9b99b68eedf16c78de5e1678
Ronald Holshausen <rholshausen@dius.com.au>
2021-09-04 12:00:28 +1000

    update to latest libs and JDK 15

--- provider/src/test/java/com/github/uglyog/mavenamqp/ProviderTest.java
+++ provider/src/test/java/com/github/uglyog/mavenamqp/ProviderTest.java
@@ -2,7 +2,7 @@ package com.github.uglyog.mavenamqp;
 
 import au.com.dius.pact.provider.PactVerifyProvider;
 import au.com.dius.pact.provider.junit.PactRunner;
-import au.com.dius.pact.provider.junit.target.AmqpTarget;
+import au.com.dius.pact.provider.junit.target.MessageTarget;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
 import au.com.dius.pact.provider.junitsupport.target.Target;
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
 @PactFolder("../consumer/target/pacts")
 public class ProviderTest {
     @TestTarget
-    public final Target target = new AmqpTarget();
+    public final Target target = new MessageTarget();
 
     @PactVerifyProvider("a test message")
     public String generateTestMessage() {
